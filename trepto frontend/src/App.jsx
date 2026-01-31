ca7d9cd2d9b55a9d35bdaebe0b04612b3746974f
JaineshOrionik <106659264+Jaineshprajapati@users.noreply.github.com>
2026-01-31 11:22:13 +0530

    Project Setup

---
+++ trepto frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+     <h1 class="text-h1 font-heading font-bold text-primary">
+  This is a 56px Title
+</h1>
+    </div>
+  );
+}
+
+export default App;
