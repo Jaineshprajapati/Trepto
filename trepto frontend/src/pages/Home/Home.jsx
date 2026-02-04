f15cc09cfc5e428baff40e8d64f4b56d2b27a607
JaineshOrionik <106659264+Jaineshprajapati@users.noreply.github.com>
2026-02-04 15:42:11 +0530

    trade finance

--- trepto frontend/src/pages/Home/Home.jsx
+++ trepto frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './components/HeroSection/HeroSection';
 import WhoWeAre from './components/Whoarewe/whoarewe';
+import TradeFinance from './components/Tradefinance/Tradefinance';
 
 const Home = () => {
   return (
  <>
  <HeroSection />
  <WhoWeAre />
+ <TradeFinance />
  </>
   );
 };
