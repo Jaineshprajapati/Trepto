29cc2bba8390498d29104dddd1e611d9dc9517e6
JaineshOrionik <106659264+Jaineshprajapati@users.noreply.github.com>
2026-02-12 11:16:05 +0530

    commiit

--- trepto frontend/src/App.jsx
+++ trepto frontend/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Ourservices />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/services" element={<Services />} /> */}
         <Route path="/career" element={<Career />} />
       </Routes>
       <Footer />
